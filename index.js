baa273a7074feb68da7295353d1cb1b90fcbe033
logic <logicfeel@nate.com>
2023-03-28 19:24:12 +0900

    index 수정

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * ES6, CJS
+ */
+const { Automation } = require('./src/automation');
+const { AutoTask } = require('./src/auto-task');
+
+
+exports.Automation = Automation;
+exports.AutoTask = AutoTask;
